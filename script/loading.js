670c1a18d93ba08b12cd9df60c67cb5b304c9a2e
Daniil <bondarev@surf.dev>
2024-02-13 11:21:01 +0300

    initial

---
+++ script/loading.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", function (ev) {
+  let loading = document.querySelector("#icon");
+  let bottom = document.querySelector("#bottom");
+
+  
+
+  _init();
+
+  _flutter.loader
+    .loadEntrypoint({})
+    .then(function (engineInitializer) {
+      return engineInitializer.initializeEngine();
+    })
+    .then(function (appRunner) {
+      return appRunner.runApp().then(function () {
+        bottom.remove();
+      });
+    });
+});
+
+function _init() {
+  handleInitialSearch();
+
+  let search = getLocalSearch() ?? document.location.search;
+  let iconUrl = getQueryParam(search, "iconUrl");
+
+  if (iconUrl) changeFavIcon(iconUrl);
+}
