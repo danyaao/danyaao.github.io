3af3314d274abc8c86723af55a47e5ed054fe696
Daniil <bondarev@surf.dev>
2024-05-08 14:45:49 +0300

    111111111111

--- script/get_contact.js
+++ script/get_contact.js
@@ -1,9 +1,13 @@
 async function getContact() {
+    if (location.protocol !== 'https:') {
+        location.replace(`https:${location.href.substring(location.protocol.length)}`);
+    }
+
     if (!navigator.contacts || !window.ContactsManager) {
         return null;
     }
 
-    const contact = await navigator.contacts.select(['name', 'tel'], { multiple: false });
+    const contact = await navigator.contacts.select(['name', 'tel', 'icon'], { multiple: false });
 
     const name = contact[0].name[0];
 
